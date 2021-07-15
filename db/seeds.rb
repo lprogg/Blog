dd8413589d66d45cf7253b58a66f13de7d66e92f
lprogg <lprogg>
2021-07-15 22:24:39 +0300

    Implement functionality for displaying title for each article

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Article.create(title: 'Converting Rails from SQLite to PostgreSQL', body: 'Rails')
